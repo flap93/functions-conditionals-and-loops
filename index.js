55fef5258137dbe112915df62dad0dfa35f1c99d
flpalacios93@gmail.comgit config --global user.name flap93git config --global user.name flpalacios93@gmail.com <flpalacios93@gmail.com>
2020-11-14 16:52:35 -0500

    secondPractice

--- index.js
+++ index.js
@@ -38,4 +38,43 @@ function greetings(helloOne) {
   
   test('apple');
 
-  testing
+
+
+  function brandsCars(cars) {
+    // extract conditions to array
+    const models = ['bmw', 'ferrari', 'audi', 'peugeot'];
+  
+    if (models.includes(cars)) {
+      console.log('nice car');
+    }
+  }
+  
+  brandsCars('bmw');
+
+
+  function test(fruit, quantity) {
+    const redFruits = ['apple', 'strawberry', 'cherry', 'cranberries'];
+  
+    // condition 1: fruit must has value
+    if (fruit) {
+      // condition 2: must be red
+      if (redFruits.includes(fruit)) {
+        console.log('red');
+  
+        // condition 3: must be big quantity
+        if (quantity > 10) {
+          console.log('big quantity');
+        }
+      }
+    } else {
+      console.log('No fruit!');
+    }
+  }
+  
+  // test results
+  test(null); // error: No fruits
+  test('apple'); // print: red
+  test('apple', 20); // print: red, big quantity
+
+  
+
