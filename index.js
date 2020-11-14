1c28c68544b21e8ce2456decb08a533740e6d953
flpalacios93@gmail.comgit config --global user.name flap93git config --global user.name flpalacios93@gmail.com <flpalacios93@gmail.com>
2020-11-14 00:00:29 -0500

    practiceOne

---
+++ index.js
@@ -0,0 +1,39 @@
+/*function strip(number) {
+    return (parseFloat(number.toPrecision(12)));
+}*/
+
+
+//conditionals 
+
+
+/*function test(fruit) {
+  if (fruit == 'apple' || fruit == 'strawberry') {
+
+    console.log('red');
+  }
+}
+
+test('strawberry');*/
+
+
+function greetings(helloOne) {
+
+    if (helloOne == 'hi' || helloOne == 'bye') {
+     console.log('Good morning');
+  
+    };
+  }
+  
+  greetings('hi');
+  
+  
+  function test(fruit) {
+    // extract conditions to array
+    const redFruits = ['apple', 'strawberry', 'cherry', 'cranberries'];
+  
+    if (redFruits.includes(fruit)) {
+      console.log('red');
+    }
+  }
+  
+  test('apple');
